8a403fb8ed3efc3fa25cdf02fbd4d75d96e92451
Sandeep Akkera <sandeep.aks@gmail.com>
2016-10-02 13:36:24 +0530

    Updated the scalaz.scala file..

--- AdvancedScala/scalaz.scala
+++ AdvancedScala/scalaz.scala
@@ -3,20 +3,28 @@ trait Monoid[T] {
 	def zero:T
 }
 
-trait FoldLeft[T {
-	def foldLeft(initVal:T, f:(A,B) => B) = {
-		
-	}
-}
-
 object DefaultImpls {
 	implicit object ListMonoid extends Monoid[String] {
 		override def compose(a:String, b:String):String = a+b
 		override def zero:String = ""
 	}
+	implicit val intMonoid:Monoid[Int] = new Monoid[Int] {
+		override def compose(a:Int, b:Int):Int = a+b
+		override def zero:Int = 0
+	}
+}
+
+trait Identity[A] {
+	val value:A
+	def plus(a:A)(implicit monoid:Monoid[A]):A = monoid.compose(value,a)
 }
 
 object MyClient {
+
+	implicit def toIdent[A](a:A):Identity[A] = new Identity[A]{
+		val value = a
+	}
+
 	implicit class ListImpl[T](xs:List[T]) {
 		def myJoinString()(implicit monoid:Monoid[T]):T = {
 			xs.foldLeft(monoid.zero)(monoid.compose)
@@ -30,6 +38,8 @@ object MyClient {
 
 import DefaultImpls._
 import MyClient._
+
+println(3.plus(4))
 
 println(MyClient.joinStrings("1"::"2"::Nil))
 
